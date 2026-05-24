edbd7ccd469bcb5a3e1c26076eb89e332793847a
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2026-05-24 19:01:58 +0100

    Add Google Maps API key to Next.js configuration
    - Updated next.config.ts to include NEXT_PUBLIC_GOOGLE_MAPS_API_KEY environment variable for Google Maps integration.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   env: {
     CACHE_GEOCODE: process.env.CACHE_GEOCODE,
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
   },
 };
 
