9c2f69677060da754f1279db8ebed5e4d81abeb9
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2026-06-05 08:43:55 +0100

    update rewrite

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   env: {
     CACHE_GEOCODE: process.env.CACHE_GEOCODE,
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5001/api/:path*',
+      },
+    ];
   },
 };
 
